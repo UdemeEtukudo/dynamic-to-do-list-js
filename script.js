42492a78267953d7eeef40ce28bfc45146f34f5c
UdemeEtukudo <udemedanieletukudo@gmail.com>
2025-06-21 11:33:47 +0100

    Update script.js
    ["taskInput.value.trim()"] - commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const addButton = document.getElementById('add-task-btn');
   const taskInput = document.getElementById('task-input');
   const taskList = document.getElementById('task-list');
+  ["taskInput.value.trim()"]
 
   // Load saved tasks
   loadTasks();
